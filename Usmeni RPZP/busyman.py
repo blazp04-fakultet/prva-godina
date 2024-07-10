396df5a9d46711b8f9229c178aea1236193fc008
Blaž Perić <blaz.peric4@gmail.com>
2024-07-10 11:44:52 +0200

    bussyMan

--- Usmeni RPZP/busyman.py
+++ Usmeni RPZP/busyman.py
@@ -1,20 +1,40 @@
 
+import copy
+
 
 def busyman(poslovi):
     p = {}
+
+    trajanje = []
+    obavljeni = 0
     for start, stop in poslovi:
-        for i in range(start, stop+1):
-            p[i] = p.get(i, 0) + 1
-    po = 0
-    for posao in p:
-        if p[posao] == 1:
-            po += 1
-    return po
+        trajanje.append(stop - start)
+
+    for _ in range(len(poslovi)):
+
+        index_min = trajanje.index(min(trajanje))
+
+        temp_p = copy.copy(p)
+        for i in range(poslovi[index_min][0], poslovi[index_min][1]+1):
+            if i in temp_p:
+                trajanje.pop(index_min)
+                poslovi.pop(index_min)
+                break
+            else:
+                temp_p[i] = 1
+                if i == poslovi[index_min][1]:
+                    poslovi.pop(index_min)
+                    trajanje.pop(index_min)
+                    obavljeni += 1
+                    p = copy.copy(temp_p)
+                    break
+
+    return obavljeni
 
 
-poslovi = [(1, 5),
+poslovi = [(1, 3),
            (4, 6),
-           (5, 10)]
+           (7, 10)]
 
 
 print(busyman(poslovi))
